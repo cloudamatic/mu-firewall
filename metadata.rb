40fa11d50fa9f11ec23f5bb7f71202b1fdb91cf6
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-24 15:27:55 -0400

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,13 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Provides a set of primitives for managing firewalls and associated rules.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.7.0'
+version '2.8.0'
 
 supports 'centos'
 supports 'debian'
 supports 'ubuntu'
 supports 'windows'
+supports 'amazon'
 
 depends 'chef-sugar'
 
