8a5a40ab5709bd298716d6a28d4cb335135e5aaa
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-24 15:27:10 -0400

    Improve support for AzL

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,8 @@
-default['firewall']['allow_ssh'] = false
+default['firewall']['allow_ssh'] = true
 default['firewall']['allow_winrm'] = false
 default['firewall']['allow_mosh'] = false
-default['firewall']['allow_loopback'] = false
+default['firewall']['allow_loopback'] = true
 default['firewall']['allow_icmp'] = false
+default['firewall']['firewalld']['permanent'] = true
+default['firewall']['ipv6_enabled'] = false
+force_default['firewall']['allow_established'] = true
